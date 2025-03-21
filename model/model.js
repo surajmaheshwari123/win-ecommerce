45a44c75fd7787bc342ff11e0dd34981f612fbc1
suraj_maheshwari <suraj.maheshwari@leverageedu.com>
2025-03-21 21:35:54 +0530

    add fields

--- model/model.js
+++ model/model.js
@@ -1,20 +1,4 @@
 const Joi = require("joi");
-
-const productSchema = Joi.object({
-    name: Joi.string().min(3).max(100).required(),
-    description: Joi.string().min(3).max(100),
-    category_id: Joi.number().integer().required(),
-    discount_price: Joi.number().positive().optional(),
-    size: Joi.string().optional(),
-    color: Joi.string().optional()
-});
-
-const categorySchema = Joi.object({
-    name: Joi.string().min(3).max(100).required(), 
-    parent_id: Joi.number().integer().allow(null),  
-    is_deleted: Joi.boolean().default(false)  
-});
-
 const variantSchema = Joi.object({
     product_id: Joi.number().integer().required(),  
     name: Joi.string().min(3).max(100).optional(),  
@@ -24,6 +8,24 @@ const variantSchema = Joi.object({
     color: Joi.string().optional(), 
     is_deleted: Joi.boolean().default(false) 
 });
+
+const productSchema = Joi.object({
+    name: Joi.string().min(3).max(100).required(),
+    description: Joi.string().min(3).max(100),
+    image_url: Joi.string().min(3).max(100),
+    category_id: Joi.number().integer().required(),
+    discount_price: Joi.number().positive().optional(),
+    size: Joi.string().optional(),
+    color: Joi.string().optional(),
+    variants: Joi.array().items(variantSchema).optional()
+});
+
+const categorySchema = Joi.object({
+    name: Joi.string().min(3).max(100).required(), 
+    parent_id: Joi.number().integer().allow(null),  
+    is_deleted: Joi.boolean().default(false)  
+});
+
 
 const validateCategory = (req, res, next) => {
     const { error } = categorySchema.validate(req.body);
